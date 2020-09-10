d07ca0e2989a2a33b9a7dcb15095b1b819ccc76b
Mikael E. Wikner <wikner.mikael@gmail.com>
2020-09-10 17:44:45 +0200

    change: added opacity threshold functionality for items

--- src/main/java/se/mickelus/mgui/gui/GuiItem.java
+++ src/main/java/se/mickelus/mgui/gui/GuiItem.java
@@ -31,12 +31,25 @@ public class GuiItem extends GuiElement {
     private boolean showTooltip = true;
     private CountMode countMode = CountMode.normal;
 
+    private float opacityThreshold = 1;
+
     public GuiItem(int x, int y) {
         super(x, y, 16, 16);
 
         mc = Minecraft.getInstance();
 
         setVisible(false);
+    }
+
+    /**
+     * Sets the opacity threshold for this element, the item will only render when the combined opacity of this element and it's parent is above the
+     * threshold.
+     * @param opacityThreshold
+     * @return
+     */
+    public GuiItem setOpacityThreshold(float opacityThreshold) {
+        this.opacityThreshold = opacityThreshold;
+        return this;
     }
 
     public GuiItem setTooltip(boolean showTooltip) {
@@ -68,17 +81,19 @@ public class GuiItem extends GuiElement {
     public void draw(MatrixStack matrixStack, int refX, int refY, int screenWidth, int screenHeight, int mouseX, int mouseY, float opacity) {
         super.draw(matrixStack, refX, refY, screenWidth, screenHeight, mouseX, mouseY, opacity);
 
-        RenderSystem.pushMatrix();
-        RenderSystem.enableDepthTest();
-        RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
-                GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        RenderHelper.enableStandardItemLighting();
-        mc.getItemRenderer().renderItemAndEffectIntoGUI(itemStack, refX + x, refY + y);
-        RenderHelper.disableStandardItemLighting();
-        mc.getItemRenderer().renderItemOverlayIntoGUI(fontRenderer, itemStack, refX + x, refY + y, getCountString());
+        if (opacity * getOpacity() >= opacityThreshold) {
+            RenderSystem.pushMatrix();
+            RenderSystem.enableDepthTest();
+            RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+                    GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+            RenderHelper.enableStandardItemLighting();
+            mc.getItemRenderer().renderItemAndEffectIntoGUI(itemStack, refX + x, refY + y);
+            RenderHelper.disableStandardItemLighting();
+            mc.getItemRenderer().renderItemOverlayIntoGUI(fontRenderer, itemStack, refX + x, refY + y, getCountString());
 
-        RenderSystem.disableDepthTest();
-        RenderSystem.popMatrix();
+            RenderSystem.disableDepthTest();
+            RenderSystem.popMatrix();
+        }
     }
 
     protected String getCountString() {
