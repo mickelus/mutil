d98cae923150433bbfa1e1f190e96e0bf5c52a01
Mikael E. Wikner <wikner.mikael@gmail.com>
2021-12-19 15:38:02 +0100

    feat: added methods for controlling shaders and blending of gui textures

--- src/main/java/se/mickelus/mutil/gui/GuiTexture.java
+++ src/main/java/se/mickelus/mutil/gui/GuiTexture.java
@@ -3,7 +3,10 @@ package se.mickelus.mutil.gui;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Supplier;
 
 public class GuiTexture extends GuiElement {
 
@@ -13,6 +16,9 @@ public class GuiTexture extends GuiElement {
     protected int textureY;
 
     protected int color = 0xffffff;
+
+    protected Supplier<ShaderInstance> shader = GameRenderer::getPositionTexShader;
+    private boolean useDefaultBlending = true;
 
     public GuiTexture(int x, int y, int width, int height, ResourceLocation textureLocation) {
         this(x, y, width, height, 0, 0, textureLocation);
@@ -38,6 +44,15 @@ public class GuiTexture extends GuiElement {
         return this;
     }
 
+    public GuiTexture setShader(Supplier<ShaderInstance> shader) {
+        this.shader = shader;
+        return this;
+    }
+
+    public void setUseDefaultBlending(boolean useDefault) {
+        this.useDefaultBlending = useDefault;
+    }
+
     @Override
     public void draw(PoseStack matrixStack, int refX, int refY, int screenWidth, int screenHeight, int mouseX, int mouseY, float opacity) {
         super.draw(matrixStack, refX, refY, screenWidth, screenHeight, mouseX, mouseY, opacity);
@@ -48,7 +63,7 @@ public class GuiTexture extends GuiElement {
 
     protected void drawTexture(PoseStack matrixStack, ResourceLocation textureLocation, int x, int y, int width, int height,
             int u, int v, int color, float opacity) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShader(shader);
         RenderSystem.setShaderColor(
                 (color >> 16 & 255) / 255f, // red
                 (color >> 8 & 255) / 255f,  // green
@@ -56,7 +71,10 @@ public class GuiTexture extends GuiElement {
                 opacity);
         RenderSystem.setShaderTexture(0, textureLocation);
         RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
+
+        if (useDefaultBlending) {
+            RenderSystem.defaultBlendFunc();
+        }
         this.blit(matrixStack, x, y, u, v, width, height);
     }
 }
